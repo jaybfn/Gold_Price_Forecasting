eb88779b92e797a435278a018bd5f8ffb3217d77
jaybfn <jayesh.bfn@gmail.com>
2022-09-05 18:30:36 +0200

    save to csv file

--- scripts/get_data_yahoo_finance.py
+++ scripts/get_data_yahoo_finance.py
@@ -1,12 +1,13 @@
 # importing necessary library
 import pandas_datareader as web
+import pandas as pd
 
 # defining a preprocessing function for the yahoofinance dataet:
 
 def preprocessing(symbol):
     
     # Importing the dataset:
-    df = web.DataReader(symbol, data_source='yahoo',start='2000-08-30', end='2022-07-22').dropna()
+    df = web.DataReader(symbol, data_source='yahoo',start='2000-08-30', end='2022-09-04').dropna()
     
     # removing the adjclose columns:
     del df['Adj Close']
@@ -16,6 +17,7 @@ def preprocessing(symbol):
 if __name__=='__main__':
 
     # Download the dataset from yfinance
-    XAUUSD = 'GC=F'
+    XAUUSD = '^GSPC'
     df = preprocessing(XAUUSD)
+    df.to_csv('../data/US500.csv')
     print(df)
